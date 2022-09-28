9fc7e49a910e7b0f04e11b3d0c8e4208fbe023fc
aravind.suri <aravind.suri@gmail.com>
2022-09-28 18:36:22 +0200

    changes to ARM scripts

---
+++ armscripts/ch1_arm_create_resources.ps1
@@ -0,0 +1,4 @@
+#-------Powershell commands-----------------------------------------------------------
+#-----Create Resource Group----------------------------------------
+
+New-AzResourceGroup -Name rg-AZDemo001 -Location "Central US"
