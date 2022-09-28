037a07d0d2b9c6837a0e0192251c0f666b756f81
aravind.suri <aravind.suri@gmail.com>
2022-09-28 18:54:01 +0200

    all changes

--- armscripts/ch1_arm_create_resources.ps1
+++ armscripts/ch1_arm_create_resources.ps1
@@ -1,4 +1,12 @@
 #-------Powershell commands-----------------------------------------------------------
 #-----Create Resource Group----------------------------------------
 
-New-AzResourceGroup -Name rg-AZDemo001 -Location "Central US"
+New-AzResourceGroup -Name rg-WineStore -Location "Central US"
+
+#-------Powershell commands-----------------------------------------------------------
+#-----Create Resources from ARM Temlplate with Parameters File------------------------
+New-AzResourceGroupDeployment     
+    -ResourceGroupName rg-WineStore
+    -TemplateFile .\ch1_arm.json     
+    -TemplateParameterFile .\ch1_arm.parameters.json
+    -Verbose
