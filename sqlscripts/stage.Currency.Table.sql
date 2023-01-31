970a4fa1c5cfddf75524a32f3f4119d20126e1f8
Aravind Suri <aravind.suri@gmail.com>
2023-01-31 17:55:28 +0100

    revised sqlscripts for stage and DW

--- sqlscripts/stage.Currency.Table.sql
+++ sqlscripts/stage.Currency.Table.sql
@@ -1,15 +1,14 @@
 USE [WineStoreDW]
 GO
 
-/****** Object:  Table [dbo].[dimCurrency]    Script Date: 10/14/2022 2:07:49 PM ******/
+/****** Object:  Table [stage].[Currency]    Script Date: 10/14/2022 2:07:49 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[dimCurrency](
-	[CurrencyId] [smallint] NOT NULL,
+CREATE TABLE [stage].[Currency](
 	[CurrencyCode] [nvarchar](3) NULL,
 	[CurrencyName] [nvarchar](50) NULL
 ) ON [PRIMARY]
