8e8e59dfc99ca9a91a7d1340194265563e42173a
Aravind Suri <aravind.suri@gmail.com>
2023-02-15 21:48:04 +0100

    changed CreateStageScehmaAndTables sqlscripts

--- sqlscripts/stage.CreateStageScehmaAndTables.Table.sql
+++ sqlscripts/stage.CreateStageScehmaAndTables.Table.sql
@@ -158,3 +158,58 @@ CREATE TABLE [stage].[Celeste_Products](
 	[Year] [smallint] NULL
 ) ON [PRIMARY]
 GO
+
+/****** Object:  Table [stage].[Arancione_Sales]    Script Date: 9/15/2022 9:22:29 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stage].[Arancione_Sales](
+	[OnlineRetailer] [nvarchar] (20) NULL,
+	[SalesMonth] [nvarchar] (12) NULL,
+	[Title] [nvarchar] (255) NULL,
+	[Vintage] [int] NULL,
+	[Variety] [nvarchar] (50) NULL,
+	[Score] [int] NULL,
+	[ListPrice] [int] NULL,
+	[Quantity] [int] NULL
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [stage].[Verde_Sales]    Script Date: 9/15/2022 9:22:29 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stage].[Verde_Sales](
+	[SalesMonth] [nvarchar] (12) NULL,
+	[OnlineRetailer] [nvarchar] (20) NULL,
+	[Title] [nvarchar] (255) NULL,
+	[Vintage] [int] NULL,
+	[Variety] [nvarchar] (50) NULL,
+	[Score] [int] NULL,
+	[Price] [int] NULL,
+	[Quantity] [int] NULL
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [stage].[Celeste_Sales]    Script Date: 9/15/2022 9:22:29 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stage].[Celeste_Sales](
+	[OnlineRetailer] [nvarchar] (20) NULL,
+	[SalesMonth] [nvarchar] (12) NULL,
+	[SalesRegion] [nvarchar] (2) NULL,
+	[SalesCurrency] [nvarchar] (3) NULL,
+	[Title] [nvarchar] (255) NULL,
+	[Vintage] [int] NULL,
+	[Variety] [nvarchar] (50) NULL,
+	[Score] [int] NULL,
+	[ListPrice] [int] NULL,
+	[Quantity] [int] NULL
+) ON [PRIMARY]
+GO
+
+
